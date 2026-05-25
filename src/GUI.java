168fa379e4b1a908720149fce51ebe647c959e6f
Mike05214 <zurlomichael6@gmail.com>
2026-05-25 16:03:56 +0200

    PROVA

---
+++ src/GUI.java
@@ -0,0 +1,35 @@
+import javax.swing.JFrame;
+import java.awt.Container;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
+
+public class GUI extends JFrame {
+    private JPanel jpanel = new JPanel(new GridLayout(0,1));
+
+    public GUI(){
+        super("Gioco");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container container = this.getContentPane();
+        container.add(jpanel,BorderLayout.CENTER);
+        jpanel.add(new JButton("Bottone"));
+        jpanel.add(new JLabel("Etichetta"));
+        this.pack();
+
+    }
+
+    public static void main (String args[]){
+
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run(){
+                new GUI().setVisible(true);
+            }
+            
+        });
+
+    }
+}
