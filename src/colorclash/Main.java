1f67641756746515128c7c68c1b9ab3f5e687981
Mike05214 <zurlomichael6@gmail.com>
2026-05-31 23:18:14 +0200

    ciao

--- src/colorclash/Main.java
+++ src/colorclash/Main.java
@@ -11,7 +11,7 @@ public class Main {
             public void run(){
                 JFrame jframe= new JFrame();
                 jframe.setVisible(true);
-                jframe.setPreferredSize(new Dimension(800, 800));
+                // ciao
             }
             
         });
