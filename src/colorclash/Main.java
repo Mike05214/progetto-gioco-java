fa413126bd852688c21b9ee43ba64fd292b8bf48
Mike05214 <zurlomichael6@gmail.com>
2026-05-31 23:05:57 +0200

    INIZIO
    Co-authored-by: Quake78 <fp00523@users.noreply.github.com>

---
+++ src/colorclash/Main.java
@@ -0,0 +1,19 @@
+package src.colorclash;
+import javax.swing.JFrame;
+import java.awt.Container;
+import java.awt.Dimension;
+import javax.swing.JButton;
+
+
+public class Main {
+    public static void main (String[] args){
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run(){
+                JFrame jframe= new JFrame();
+                jframe.setVisible(true);
+            }
+            
+        });
+        
+    }
+}
