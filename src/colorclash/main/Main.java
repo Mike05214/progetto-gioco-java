1d544e17027f13e51bbfca8737d3c44b610fe5ed
Mike05214 <zurlomichael6@gmail.com>
2026-06-08 11:26:26 +0200

    correzione bug windows

--- src/colorclash/main/Main.java
+++ src/colorclash/main/Main.java
@@ -4,6 +4,9 @@ import src.colorclash.view.MainFrame;
 
 public class Main {
     public static void main (String[] args){
+        // PREVIENE IL BUG DEL CLEARTYPE DI WINDOWS, "Usa il tuo anti-aliasing interno e non fare domande a Windows".
+        System.setProperty("awt.useSystemAAFontSettings", "on");
+        System.setProperty("swing.aatext", "true");
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run(){
                 MainFrame mainFrame= new MainFrame();
