3f843d03cadbb089ca52047487fea11505724c05
Mike05214 <zurlomichael6@gmail.com>
2026-06-21 23:44:41 +0200

    dimensione sinusoidal

--- src/colorclash/model/SinusoidalMadness.java
+++ src/colorclash/model/SinusoidalMadness.java
@@ -1,13 +1,17 @@
 package src.colorclash.model;
 import java.awt.Shape;
 import java.awt.geom.Ellipse2D;
+import java.util.Random;
 
 public class SinusoidalMadness extends Obstacle {
+    private static final int WIDTH = 70;
+    private static final int HEIGHT = 70;
+    private static final Random random = new Random();
   
     public SinusoidalMadness(int startX, int startY, double speed, int colorId,int width,int height) {
         
         
-        super(startX, startY, speed, colorId,width,height);
+        super(startX, startY, speed, colorId,WIDTH,HEIGHT);
         
        
     }
