c87f834cb06ff01862f165cc85a9d8d5b60cfc5a
Mike05214 <zurlomichael6@gmail.com>
2026-07-24 13:37:51 +0200

    windows classic

--- src/colorclash/main/Main.java
+++ src/colorclash/main/Main.java
@@ -3,6 +3,7 @@ package src.colorclash.main;
 import src.colorclash.view.MainFrame;
 import src.colorclash.utils.AudioManager;
 
+import javax.swing.BorderFactory;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 import java.awt.Color;
@@ -19,7 +20,7 @@ public class Main {
         try {
             // Scegli il tema di Swing che vuoi testare cambiando questa stringa.
             // Puoi provare: "Nimbus", "Metal", oppure "CDE/Motif"
-            String temaScelto = "CDE/Motif";
+            String temaScelto = "Windows Classic";
 
             System.out.println("Temi disponibili su questo PC:");
             for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
@@ -38,6 +39,7 @@ public class Main {
             UIManager.put("Button.foreground", Color.WHITE);
             UIManager.put("Button.font", new Font("Arial", Font.BOLD, 16));
             UIManager.put("Button.focus", new Color(0, 0, 0, 0));
+            
 
         } catch (Exception e) {
             System.err.println("Errore durante l'impostazione del Look and Feel.");
